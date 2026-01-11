0c30b69865b0e80d082aab916354404c31cf40ca
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-01-10 20:31:06 -0500

    implementacion del coponente Button

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,6 @@
+import { Button } from './Button'
+import { FaCartShopping } from "react-icons/fa6";
+
 export const ProductCard = ({ img, altImg, name, price }) => {
     return (
         <div className="group flex w-60 flex-col gap-3">
@@ -10,9 +13,9 @@ export const ProductCard = ({ img, altImg, name, price }) => {
                     <p className="font-bold text-primary">${price}</p>
                 </div>
 
-                <button className="flex h-9 w-9 items-center justify-center rounded-full bg-primary text-white/70 hover:bg-[color-mix(in_srgb,var--primary,black_20%)] transition-transform">
-                    <span className="flex items-center justify-center text-4xl -mt-1.5 text-black">+</span>
-                </button>
+            <Button Icon={FaCartShopping
+                
+            }/>              
             </div>
         </div>
     );
