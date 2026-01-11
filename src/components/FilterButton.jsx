a07f2a6106375b1487dda36294e53a3d8530c998
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-01-11 13:56:46 -0500

    FilterButton mas grande

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -2,7 +2,7 @@ export const FilterButton = ({ Icon, text = "" }) => {
     return (
         <button className="group flex p-1 lg:mb-2 items-center justify-center gap-x-2 rounded-full border border-white/20 bg-white/5 px-4 transition-transform active:scale-95 hover:border-white focus:outline-none focus:ring-0 focus:bg-primary active:ring-0">
             {Icon && <Icon className="text-white group-focus:text-black" />}
-            <span className="text-xs lg:text-base font-bold text-gray-300 group-focus:text-black select-none">
+            <span className="text-sm lg:text-base font-bold text-gray-300 group-focus:text-black select-none">
                 {text}
             </span>
         </button>
