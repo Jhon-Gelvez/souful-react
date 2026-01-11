e6e9841da95110136d700a8f85c9e3434ac0fc5c
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-01-10 20:32:10 -0500

    responsive para pc

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,8 +1,8 @@
 export const FilterButton = ({ Icon, text = "" }) => {
     return (
-        <button className="group flex p-1 items-center justify-center gap-x-2 rounded-full border border-white/20 bg-white/5 px-4 transition-transform active:scale-95 hover:border-white focus:outline-none focus:ring-0 focus:bg-primary active:ring-0">
+        <button className="group flex p-1 lg:mb-2 items-center justify-center gap-x-2 rounded-full border border-white/20 bg-white/5 px-4 transition-transform active:scale-95 hover:border-white focus:outline-none focus:ring-0 focus:bg-primary active:ring-0">
             {Icon && <Icon className="text-white group-focus:text-black" />}
-            <span className="text-xs font-bold text-gray-300 group-focus:text-black select-none">
+            <span className="text-xs lg:text-base font-bold text-gray-300 group-focus:text-black select-none">
                 {text}
             </span>
         </button>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,14 @@ export default function Header() {
     return (
         <header id="inicio" className="flex items-center justify-between bg-transparent px-4 py-3">
             <div>
-                <h2 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">SoulfulArt</h2>
-                <p className="text-xs font-medium text-white/80">Arte hecho con el alma</p>
+                <h1 className="text-2xl lg:text-4xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">SoulfulArt</h1>
+                <p className="text-xs lg:text-sm font-medium text-white/80">Arte hecho con el alma</p>
             </div>
             <div
                 className="
             w-fit mr-auto ml-4 border border-white/40 rounded-full"
             >
-                <img src={logo} alt="" className="w-14 rounded-full p-0" />
+                <img src={logo} alt="" className="w-14 lg:w-18 rounded-full p-0" />
             </div>
             <div className="flex items-center gap-3">
                 <Button Icon={HiSearch} />
