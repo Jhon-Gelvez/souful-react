9c1f03f43d08c6bd286957fa50f8da6b626e26b1
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-01-22 15:33:35 -0500

    --

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ export default function App() {
             <FilterBar />
             <ProductGrid />
             <BottomNavbar />
-            <Cloudinary />
             <Footer />
         </>
     );
