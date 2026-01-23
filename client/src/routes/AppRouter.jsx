0b6dfc6152133fd988c1f8bc1ea3d36b72b820f3
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-01-23 15:46:35 -0500

    nueva estructura de carpetas y el nav de appRouter esta commetado

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -3,12 +3,11 @@ import { Home } from "../UI/pages/Home";
 import { Admin } from "../UI/pages/Admin";
 
 export function AppRouter() {
-    console.log("omaga")
     return (
         <Router>
-            <nav className='text-white text-6xl font-bold'>
-                <Link to="/souful-react/">Inicio</Link> | <Link to="/admin">Admin</Link>
-            </nav>
+            {/* <nav className='text-white text-xl font-bold m-4'>
+                <Link to="/souful-react/">Inicio</Link> <Link to="/admin">Admin</Link>
+            </nav> */}
             <Routes>
                 <Route path="/souful-react/" element={<Home />} />
                 <Route path="/admin" element={<Admin />} />
