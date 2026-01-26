69cf67db455e24cd7fbe6e89a1032590c3cc59f1
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-01-26 08:38:45 -0500

    orden en las clases

--- client/src/UI/components/Button.jsx
+++ client/src/UI/components/Button.jsx
@@ -2,13 +2,13 @@ export const Button = ({ Icon }) => {
     return (
         <button
             className="group flex h-10 w-10 lg:w-15 items-center justify-center rounded-full 
-             bg-white/10 border border-transparent
+             bg-white/10 border border-transparent cursor-pointer
              hover:bg-blue-500/20 transition-all 
-             focus:outline-none hover:border-white focus:ring-0
-             active:scale-95 active:border-white focus:bg-primary cursor-pointer "
+             focus:outline-none hover:border-white focus:ring-0 focus:bg-primary
+             active:scale-95 active:border-white"
             aria-label="Buscar"
         >
-            <Icon className="drop-shadow-[0_0_1px_rgba(0,0,0,0.5)] text-zinc-800 dark:text-white group-focus:text-black text-2xl transition-colors " />
+            <Icon className="drop-shadow-[0_0_1px_rgba(0,0,0,0.5)] text-zinc-800 dark:text-white group-focus:text-black transition-colors" />
         </button>
     );
 };
