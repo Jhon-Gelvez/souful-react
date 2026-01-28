f194933a1feb06cce07991ebd0d533ceb69abc09
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-01-28 11:55:51 -0500

    logs

--- server/services/deleteImage.js
+++ server/services/deleteImage.js
@@ -1,10 +1,12 @@
-import { configCloudinary } from '../config/cloudinary.js'
+import { cloudinary } from "../config/cloudinary.js";
 
 export const deleteImage = async (publicId) => {
-  try {
-    const result = await configCloudinary.uploader.destroy(publicId);
-    return result;
-  } catch (error) {
-    throw new Error('Error al eliminar en Cloudinary ', error);
-  }
+    try {
+        const result = await cloudinary.uploader.destroy(publicId);
+        console.log("Respuesta de Cloudinary:", result);
+        return result;
+    } catch (error) {
+        console.error("Detalle técnico del error en Cloudinary:", error);
+        throw new Error("Error al eliminar en Cloudinary ", error);
+    }
 };
