6697865284d10fbecb443503cdc021c6fb82a517
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-01-28 14:43:43 -0500

    get item

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
+import * as itemController from "../controller/itemController.js";
+
 const router = express.Router();
 
-// IMPORTANT: Always include the .js extension for local imports in ES modules
-import * as itemController from '../controller/itemController.js';
-
-router.get('/', itemController.getAllItemsDB);
-router.post('/', itemController.createItemDB);
-router.delete('/:public_id', itemController.deleteItemDB_cdl);
+router.get("/:public_id", itemController.getItem)
+router.get("/", itemController.getAllItemsDB);
+router.post("/", itemController.createItemDB);
+router.delete("/:public_id", itemController.deleteItemDB_cdl);
 
 export default router;
