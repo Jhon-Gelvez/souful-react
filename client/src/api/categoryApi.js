b564be11ece598717afa36e4f39df5bc73e9bcca
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-02-10 08:48:16 -0500

    api - no implementado

---
+++ client/src/api/categoryApi.js
@@ -0,0 +1,5 @@
+export const getCategory = () => {}
+export const listCategories = () => {}
+export const createCategory = () => {}
+export const updateCategory = () => {}
+export const deleteCategory = () => {}
---
+++ client/src/api/itemApi.js
@@ -0,0 +1,81 @@
+export const getItem = async (public_id) => {
+    try {
+        const response = await fetch(`localhost:3001/api/items/${public_id}`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (!response.ok) {
+            throw new Error("Error al obtener el item");
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
+export const listItems = async () => {
+    try {
+        const response = await fetch(`localhost:3001/api/items`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (!response.ok) {
+            throw new Error("Error al obtener el item");
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
+export const createItem = async (itemData) => {
+    try {
+        const response = await fetch(`localhost:3001/api/items`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(itemData)
+        });
+
+        if (!response.ok) {
+            throw new Error("Error al obtener el item");
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
+export const updateItem = async (public_id, itemData) => {
+ try {
+        const response = await fetch(`localhost:3001/api/items/${public_id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(itemData)
+        });
+
+        if (!response.ok) {
+            throw new Error("Error al obtener el item");
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
+export const deleteItem = async (publicId) => {
+     try {
+        const response = await fetch(`localhost:3001/api/items`, {
+            method: "DELETE",
+        });
+
+        if (!response.ok) {
+            throw new Error("Error al obtener el item");
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
