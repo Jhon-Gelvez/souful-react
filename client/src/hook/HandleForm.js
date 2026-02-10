3d1c5e48b597aee62b2f0f16b62c87da60fdc501
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-02-10 08:49:06 -0500

    se le pasan la metadata de otra manera

--- client/src/hook/HandleForm.js
+++ client/src/hook/HandleForm.js
@@ -41,15 +41,29 @@ export const HandleForm = () => {
         try {
             // PASO A: Subir imagen y obtener URL
             console.log("Subiendo imagen...");
-            const imageUrl = await uploadImage(selectedFile, {
-                title: formData.title,
-                description: formData.description,
-                category: formData.category,
-            });
+            const cloudinaryData = await uploadImage(selectedFile, formData);
 
+            // TODO
             // PASO B: Enviar todo a tu API Rest
-            const finalData = { ...formData, image: imageUrl };
-            console.log("Enviando al backend (no implementado):", finalData);
+            const bodyForDB = {
+                name_product: cloudinaryData.context.custom.caption,
+                alt: cloudinaryData.context.custom.alt, 
+                price: parseInt(formData.price),
+                image_url: cloudinaryData.optimized_url,
+                public_id: cloudinaryData.public_id,
+                file_size: parseInt(cloudinaryData.bytes),
+                mime_type: `${cloudinaryData.resource_type}/${cloudinaryData.format}`,
+                dimensions: `${cloudinaryData.width}x${cloudinaryData.height}`,
+                // fix
+                // solo toma la primera categoria del producto
+                // toma el nombre que se pasa del form pero necesitamos el id
+                category_id: `${cloudinaryData.tags[0]}`,
+            };
+
+            // peticion a al api
+
+            console.log(`Datos para enviar al backend ${JSON.stringify(bodyForDB) }`);
+
             alert("¡Producto y metadata guardados!");
             // Aquí iría tu fetch('/api/products', { method: 'POST', ... })
         } catch (error) {
