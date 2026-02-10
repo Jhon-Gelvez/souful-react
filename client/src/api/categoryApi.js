1dc9c8beb3920ff95c6146d517985572582a7297
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-02-10 11:07:19 -0500

    creada la primera api rest para listar categorias

--- client/src/api/categoryApi.js
+++ client/src/api/categoryApi.js
@@ -1,5 +1,15 @@
-export const getCategory = () => {}
-export const listCategories = () => {}
-export const createCategory = () => {}
-export const updateCategory = () => {}
-export const deleteCategory = () => {}
+const URL = import.meta.env.VITE_URL;
+
+export const getCategory = async () => {};
+export const listCategories = async () => {
+    try {
+        const response = await fetch(`${URL}/api/categories`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error cargando categorías:", error);
+    }
+};
+export const createCategory = async () => {};
+export const updateCategory = async () => {};
+export const deleteCategory = async () => {};
