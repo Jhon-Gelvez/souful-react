7f5e55362fb296294a18dfcf90342f78259a4206
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-02-16 15:05:59 -0500

    sin el outline al hacer focus

--- client/src/UI/components/admin/Input.jsx
+++ client/src/UI/components/admin/Input.jsx
@@ -1,3 +1,3 @@
-export const Input = ({name="", setOonChange=null,type="text"}) => {
-    return <input required onChange={setOonChange} name={name} className="border border-white  rounded-lg py-1 pl-1 caret-white text-white mb-2 w-full no-spinner" type={type} />;
+export const Input = ({ name = "", setOonChange = null, type = "text" }) => {
+    return <input required onChange={setOonChange} name={name} className="border border-white  rounded-lg py-1 pl-1 caret-white text-white mb-2 w-full no-spinner focus:outline-none" type={type} />;
 };
