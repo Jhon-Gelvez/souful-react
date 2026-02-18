cf347cfea8da0b285bcf242ee88ac21b2b861008
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-02-18 15:07:11 -0500

    soporta todos sus atributos como props opcionales

--- client/src/UI/components/admin/Input.jsx
+++ client/src/UI/components/admin/Input.jsx
@@ -1,3 +1,3 @@
-export const Input = ({ name = "", setOnChange = null, type = "text" }) => {
-    return <input required onChange={setOnChange} name={name} className="border border-white  rounded-lg py-1 pl-1 caret-white text-white mb-2 w-full no-spinner focus:outline-none" type={type} />;
+export const Input = ({ name = "", setOnChange = null, type = "text", id = "", placeholder = "" }) => {
+    return <input required onChange={setOnChange} id={id} name={name} type={type} placeholder={placeholder} className="border border-white  rounded-lg py-1 pl-1 caret-white text-white mb-2 w-full no-spinner focus:outline-none" />;
 };
