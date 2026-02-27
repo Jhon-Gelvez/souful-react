22a0c80ffa87820aac191f18fba6b54c5a87f0dc
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-02-27 09:41:53 -0500

    cambio en formato

--- client/src/UI/components/common/Button.jsx
+++ client/src/UI/components/common/Button.jsx
@@ -1,12 +1,12 @@
-export const Button = ({ Icon, onClick=null}) => {
+export const Button = ({ Icon, onClick = null }) => {
     return (
-        <button onClick={onClick}
+        <button
+            onClick={onClick}
             className={`group flex h-12 w-12 items-center justify-center  
              bg-white/10 border border-transparent cursor-pointer rounded-full
              hover:bg-blue-500/20 transition-all 
              focus:outline-none hover:border-white focus:ring-0 focus:bg-primary
              active:scale-95 active:border-white`}
-            
         >
             <Icon className="drop-shadow-[0_0_1px_rgba(0,0,0,0.5)] text-zinc-800 dark:text-white group-focus:text-black transition-colors text-3xl lg:text-2xl" />
         </button>
