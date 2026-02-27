1211508973ea9e4889870f63c4f1e5674c21c929
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2026-02-27 09:41:18 -0500

    componente editar producto dinamico

--- client/src/UI/components/admin/EditForm.jsx
+++ client/src/UI/components/admin/EditForm.jsx
@@ -1,31 +1,29 @@
+import React from "react";
 import { Label } from "./Label";
 import { Input } from "./Input";
 import { Button } from "../common/Button";
 import { FiCheck } from "react-icons/fi";
 
-export const EditForm = ({ name, price, onSubmit }) => {
+// pensar en como hacer esto y pasarle bien las props para que sea dinamico
+
+export const EditForm = ({ InputSettings, onSubmit, onChange }) => {
+    console.log(InputSettings);
     return (
         <div className="flex flex-col justify-center items-start text-primary mx-auto shadow-[0_0_3rem_rgba(0,0,0)] rounded-xl p-4 w-sm md:w-md px-9">
             <h1 className="text-3xl font-bold  my-2 self-center">Editar producto</h1>
-            <Label text={"Nombre del producto"} />
+            {InputSettings.map((input) => (
+                // Importante: El Fragment <></> necesita una 'key' si está dentro de un map
+                <React.Fragment key={input.id}>
+                    <Label htmlFor={input.id} text={input.textLabel || input.name} />
+                    <Input
+                        {...input} // <--- ¡MAGIA! Esto pasa id, name, placeholder, type, etc. de un solo golpe
+                        setOnChange={onChange} // Pasamos la referencia de la función directamente
+                    />
+                </React.Fragment>
+            ))}
 
-            <Input
-                type="text"
-                name={name}
-                setOnChange={() => {
-                    console.log("hola mundo");
-                }}
-            />
-            <Label text={"Descripcion del producto"} />
-            <Input
-                type="text"
-                name={name}
-                setOnChange={() => {
-                    console.log("hola mundo");
-                }}
-            />
             <div className="flex justify-center items-center w-full mt-2">
-                <Button Icon={FiCheck} onClick={onSubmit}/>
+                <Button Icon={FiCheck} onClick={onSubmit} />
             </div>
         </div>
     );
--- client/src/UI/components/admin/ItemManager.jsx
+++ client/src/UI/components/admin/ItemManager.jsx
@@ -1,17 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { getItem, listItems, createItem, updateItem, deleteItem } from "../../../api/itemApi";
 import { SearchForm } from "./SearchForm";
 import { InfoItem } from "./InfoItem";
 import { EditForm } from "./EditForm";
 
+// estructura de un item
+// alt
+// category_id
+// created_at
+// dimensions
+// file_size
+// id
+// image_url
+// is_active
+// mime_type
+// name_product
+// price
+// public_id
+// updated_at
+
 export const ItemManager = () => {
     const [editingin, setEditingin] = useState(false);
     const [results, setResults] = useState([]);
     const [singleItem, setSingleItem] = useState(null);
-
     // Estado para el formulario (Crear y Editar)
-    const [formData, setFormData] = useState({ name_product: "", price: "", image_url: "" });
+    const [formData, setFormData] = useState({ name_product: "", price: "", public_id: "" });
     const [editingId, setEditingId] = useState(null);
+    // input settings
+    // pasa dinamicamente un array con los input que se deben render en el formulario de edicion
+    // el type del input esta hardodeado
+    const [InputSettings, setInputSettings] = useState([]);
+    
+    useEffect(() => {
+        if (singleItem !== null) {
+            setInputSettings([
+                { htmlFor: singleItem.name_product, textLabel: "Nombre del producto", id: singleItem.name_product, name: singleItem.name_product, placeholder: "name_product", type: "text" },
+                { htmlFor: singleItem.alt, textLabel: "Descripcion", id: singleItem.alt, name: "alt", placeholder: singleItem.alt, type: "number" },
+                { htmlFor: singleItem.price, textLabel: "Precio", id: singleItem.price, name: "price", placeholder: singleItem.price, type: "number" },
+            ]);
+        }
+    }, [singleItem]);
 
     // --- OPERACIONES ---
 
@@ -26,9 +54,16 @@ export const ItemManager = () => {
 
     const handleEdit = (item) => {
         setEditingin(true);
+        setSingleItem(item);
         setResults([]);
         setEditingId(item.public_id || item.id);
-        setFormData({ name_product: item.name_product, price: item.price });
+
+        // obtener los valores desdes las llamadas a componentes anidados
+        setFormData({ name_product: "", price: "", public_id: "" });
+    };
+
+    const handleOnChange = (e) => {
+        setNombre(e.target.value);
     };
 
     const handleCopy = (text) => {
@@ -59,51 +94,18 @@ export const ItemManager = () => {
         e.preventDefault();
         if (editingId) {
             // Actualizar
+            console.log(formData);
             const response = await updateItem(editingId, formData);
             if (!response.ok) console.error(response);
+            alert("item actualizado");
             setEditingId(null);
-        } else {
-            // Crear
-            await createItem(formData);
         }
         setFormData({ name_product: "", price: "" });
-        handleListAll(); // Refrescar lista
+        handleSearch(formData.public_id);
     };
 
     return (
         <div className="mx-auto p-6 space-y-6">
-            {/* 1. Formulario de Creación / Edición */}
-            {/* <div className="p-6 bg-white shadow-lg rounded-xl border border-gray-100">
-                <h2 className="text-xl font-bold mb-4 text-gray-800">
-                    {editingId ? 'Editar Producto' : 'Nuevo Producto'}
-                </h2>
-                <form onSubmit={handleSubmit} className="flex flex-col md:flex-row gap-4">
-                    <input 
-                        type="text" 
-                        placeholder="Nombre del producto"
-                        className="flex-1 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-emerald-500 outline-none"
-                        value={formData.name_product}
-                        onChange={(e) => setFormData({...formData, name_product: e.target.value})}
-                        required
-                    />
-                    <input 
-                        type="number" 
-                        placeholder="Precio"
-                        className="w-full md:w-32 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-emerald-500 outline-none"
-                        value={formData.price}
-                        onChange={(e) => setFormData({...formData, price: e.target.value})}
-                        required
-                    />
-                    <button type="submit" className={`px-6 py-2 rounded-lg text-white font-bold transition-all ${editingId ? 'bg-orange-500 hover:bg-orange-600' : 'bg-emerald-600 hover:bg-emerald-700'}`}>
-                        {editingId ? 'Actualizar' : 'Guardar'}
-                    </button>
-                    {editingId && (
-                        <button onClick={() => {setEditingId(null); setFormData({name_product:'', price:''})}} className="text-gray-500 underline text-sm">Cancelar</button>
-                    )}
-                </form>
-            </div> */}
-
-            {/* 2. Búsqueda y Listado */}
             <div className="flex flex-col justify-center items-center w-fit text-primary mx-auto shadow-[0_0_3rem_rgba(0,0,0)] rounded-xl p-4 ">
                 <SearchForm onSearch={handleSearch} onListAll={handleListAll} />
 
@@ -115,15 +117,15 @@ export const ItemManager = () => {
                     {/* Caso: Lista completa */}
                     <ul>
                         {results &&
-                            results.map((item) => (
+                            results.map((item, i) => (
                                 <li key={item.public_id}>
-                                    <InfoItem name={item.name_product} price={item.price} public_id={item.public_id} image_url={item.image_url} onCopy={handleCopy} onDelete={handleDelete} onEdit={handleEdit} singleItem={singleItem} />
+                                    <InfoItem name={item.name_product} price={item.price} public_id={item.public_id} image_url={item.image_url} onCopy={handleCopy} onDelete={handleDelete} onEdit={handleEdit} singleItem={results[i]} />
                                 </li>
                             ))}
                     </ul>
                 </div>
             </div>
-            <div>{editingin && <EditForm />}</div>
+            <div>{editingin && <EditForm InputSettings={InputSettings} onSubmit={handleSubmit} onChange={handleOnChange} />}</div>
         </div>
     );
 };
