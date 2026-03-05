961fa0d9db471c1ec419803d1877005546f9313e
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-03-05 09:19:52 -0500

    son log y usa el index como key

--- client/src/UI/components/admin/EditForm.jsx
+++ client/src/UI/components/admin/EditForm.jsx
@@ -7,17 +7,16 @@ import { FiCheck } from "react-icons/fi";
 // pensar en como hacer esto y pasarle bien las props para que sea dinamico
 
 export const EditForm = ({ InputSettings, onSubmit, onChange }) => {
-    console.log(InputSettings);
     return (
         <div className="flex flex-col justify-center items-start text-primary mx-auto shadow-[0_0_3rem_rgba(0,0,0)] rounded-xl p-4 w-sm md:w-md px-9">
             <h1 className="text-3xl font-bold  my-2 self-center">Editar producto</h1>
-            {InputSettings.map((input,i) => (
+            {InputSettings.map((input, i) => (
                 // Importante: El Fragment <></> necesita una 'key' si está dentro de un map
                 <React.Fragment key={i}>
                     <Label htmlFor={input.id} text={input.textLabel || input.name} />
                     <Input
-                        {...input} // <--- ¡MAGIA! Esto pasa id, name, placeholder, type, etc. de un solo golpe
                         setOnChange={onChange} // Pasamos la referencia de la función directamente
+                        {...input} // <--- ¡MAGIA! Esto pasa id, name, placeholder, type, etc. de un solo golpe
                     />
                 </React.Fragment>
             ))}
