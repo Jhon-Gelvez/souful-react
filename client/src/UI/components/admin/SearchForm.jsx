efd9c522a41d251f6ad3252427ff6897b750c357
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-03-08 18:26:42 -0500

    no wrap text

--- client/src/UI/components/admin/SearchForm.jsx
+++ client/src/UI/components/admin/SearchForm.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { ButtonRectangular } from "../common/ButtonRectangular";
 
-export const SearchForm = ({textLabel, onSearch, onListAll }) => {
+export const SearchForm = ({ textLabel, onSearch, onListAll, create = false, onCreate = null }) => {
     const [searchId, setSearchId] = useState("");
 
     return (
         <div className="flex flex-col md:flex-row justify-between items-start md:items-end gap-4 mb-8">
             <div className="w-full md:w-1/2 space-y-2">
-                <label className="text-md font-bold text-white underline">{textLabel}</label>
+                <label className="text-md font-bold text-white underline whitespace-nowrap">{textLabel}</label>
                 <div className="flex gap-2 mt-1.5">
                     <input type="text" className="flex-1 px-4 py-2 border border-primary rounded-lg  focus:ring-2    outline-none" placeholder="ID del producto..." value={searchId} onChange={(e) => setSearchId(e.target.value)} />
                     <ButtonRectangular
@@ -24,6 +24,14 @@ export const SearchForm = ({textLabel, onSearch, onListAll }) => {
                         onListAll();
                     }}
                 />
+                {create && (
+                    <ButtonRectangular
+                        text={"Crear categoria"}
+                        handleClick={() => {
+                            onCreate();
+                        }}
+                    />
+                )}
             </div>
         </div>
     );
