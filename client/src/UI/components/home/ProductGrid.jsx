b8a18452cd6f913ca8ce327595281115676a49ed
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-03-13 09:57:25 -0500

    render dinamico desde db

--- client/src/UI/components/home/ProductGrid.jsx
+++ client/src/UI/components/home/ProductGrid.jsx
@@ -1,22 +1,26 @@
 // meter un main y darle grid con minimo 1 col max las que quepan usarndo repeat
+import { useState, useEffect } from "react";
 import { ProductCard } from "./ProductCard";
-import img2 from "../../../asset/portada-tejidos.jpg";
-import img from "../../../asset/portada-bisuteria.jpg";
+import { listItems } from "../../../api/itemApi";
 
 export const ProductGrid = () => {
+    // 1. Usar useState para que React re-renderice cuando cambien los datos
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        const getImages = async () => {
+            const results = await listItems();
+            setImages(results); // Actualizamos el estado
+        };
+        getImages();
+    }, []);
+
     return (
         <main className="w-full p-2 ">
             <div className="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-x-4 gap-y-6 justify-items-center">
-                <ProductCard img={img} altImg={"manilla de loro hecha en telar"} name={"Manilla de loro"} price={"15.000"} />
-                <ProductCard img={img2} altImg={"manilla de loro hecha en telar"} name={"Manilla de loro"} price={"15.000"} />
-                <ProductCard img={img} altImg={"manilla de loro hecha en telar"} name={"Manilla de loro"} price={"15.000"} />
-                <ProductCard img={img} altImg={"manilla de loro hecha en telar"} name={"Manilla de loro"} price={"15.000"} />
-                <ProductCard img={img2} altImg={"manilla de loro hecha en telar"} name={"Manilla de loro"} price={"15.000"} />
-                <ProductCard img={img} altImg={"manilla de loro hecha en telar"} name={"Manilla de loro"} price={"15.000"} />
-                <ProductCard img={img} altImg={"manilla de loro hecha en telar"} name={"Manilla de loro"} price={"15.000"} />
-                <ProductCard img={img} altImg={"manilla de loro hecha en telar"} name={"Manilla de loro"} price={"15.000"} />
-                <ProductCard img={img2} altImg={"manilla de loro hecha en telar"} name={"Manilla de loro"} price={"15.000"} />
-                <ProductCard img={img} altImg={"manilla de loro hecha en telar"} name={"Manilla de loro"} price={"15.000"} />
+                {images.map((image) => (
+                    <ProductCard key={image.id} img={image.image_url} altImg={image.alt} name={image.name_product} price={image.price} />
+                ))}
             </div>
         </main>
     );
