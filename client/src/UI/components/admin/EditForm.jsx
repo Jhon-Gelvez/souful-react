faba66ab6d5129712a29ccdbac98001cfbdd6fc2
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-03-13 09:29:30 -0500

    formiulario de edicion con el parametro opc 'label'

--- client/src/UI/components/admin/EditForm.jsx
+++ client/src/UI/components/admin/EditForm.jsx
@@ -5,11 +5,10 @@ import { Button } from "../common/Button";
 import { FiCheck } from "react-icons/fi";
 
 // pensar en como hacer esto y pasarle bien las props para que sea dinamico
-
-export const EditForm = ({ InputSettings, onSubmit, onChange }) => {
+export const EditForm = ({label="", InputSettings, onSubmit, onChange }) => {
     return (
         <div className="flex flex-col justify-center items-start text-primary mx-auto shadow-[0_0_3rem_rgba(0,0,0)] rounded-xl p-4 w-sm md:w-md px-9">
-            <h1 className="text-3xl font-bold  my-2 self-center">Editar producto</h1>
+            <h1 className="text-3xl font-bold  my-2 self-center">{label || "Editar producto"}</h1>
             {InputSettings.map((input, i) => (
                 // Importante: El Fragment <></> necesita una 'key' si está dentro de un map
                 <React.Fragment key={i}>
