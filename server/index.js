6ad78cf67ff5aff904af7791c57d565529f6130e
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-03-30 11:53:38 -0500

    cambio en el nombre de un import y peticion a / creada

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import router from "./routes/itemRoutes.js";
-import categoryRoutes from './routes/categoryRoutes.js'
+import itemRoutes from "./routes/itemRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 
 const app = express();
 
@@ -10,8 +10,13 @@ app.use(cors());
 app.use(express.json()); // Para poder leer JSON en los POST
 
 // --- RUTAS ---
+// ruta de prueba
+app.use("/", (req, res) => {
+    res.json({ message: "server running on port 3001" });
+});
+
 // Usar las rutas
-app.use("/api/items", router);
+app.use("/api/items", itemRoutes);
 
 app.use("/api/categories", categoryRoutes);
 
