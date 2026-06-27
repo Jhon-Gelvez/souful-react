7dce3509e4f830dece5ddb43d33bb50470be4ce4
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-06-27 14:03:13 -0500

    merge modal view con data base sync

---
+++ database/consultas.sql
@@ -0,0 +1,8 @@
+use soulfulart;
+
+SELECT * FROM product_images;
+
+UPDATE product_images
+SET image_url = "https://res.cloudinary.com/dnucajsxn/image/upload/v1782086376/gemy5soddxecn0atff3d.jpg"
+where id = "9f2a713f-fe00-4583-8795-326d67c9b188";
+
