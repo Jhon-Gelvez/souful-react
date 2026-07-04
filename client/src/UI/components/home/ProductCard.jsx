0e49e8a63c3d52c1892feee5d05761f73c96752c
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-07-04 15:13:48 -0500

    preview dev icono en product card

--- client/src/UI/components/home/ProductCard.jsx
+++ client/src/UI/components/home/ProductCard.jsx
@@ -4,6 +4,14 @@ import { FaCartShopping } from "react-icons/fa6";
 import { modalProductContext } from "../../../context/modalProductContext.js";
 import { modalShoppingContext } from "../../../context/modalShoppingContext.js";
 
+import { FilterButton } from "./FilterButton";
+import { GiButterfly } from "react-icons/gi";
+import { LuBaby } from "react-icons/lu";
+import { SiCrunchyroll } from "react-icons/si";
+import { MdOutlineCatchingPokemon } from "react-icons/md";
+// <FilterButton Icon={GiButterfly} text="Animales" />
+// <FilterButton Icon={LuBaby} text="Muñecas" />
+
 export const ProductCard = ({ item }) => {
     const { openModal: openProductModal } = useContext(modalProductContext);
     const { openModal: openShoppingModal } = useContext(modalShoppingContext);
@@ -24,6 +32,7 @@ export const ProductCard = ({ item }) => {
             <div className="flex items-center justify-between px-1">
                 <div className="flex-1 min-w-0">
                     <h3 className="font-semibold text-gray-900 dark:text-white line-clamp-1 text-lg tracking-wide">{item.name_product}</h3>
+                    <MdOutlineCatchingPokemon className="text-white group-focus:text-black" />
                     <p className="font-bold text-primary">${item.price}</p>
                 </div>
                 <Button
