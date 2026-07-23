4590751ebf1afd442257de7ed1f6f760fd3cd7bf
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-07-23 17:16:54 -0500

    fix(UI): cambiar keys de los objetos

--- client/src/UI/components/home/ProductCard.jsx
+++ client/src/UI/components/home/ProductCard.jsx
@@ -26,8 +26,8 @@ export const ProductCard = ({ item }) => {
 
             <div className="flex items-center justify-between px-1">
                 <div className="flex-1 min-w-0">
-                    <h3 className="font-semibold text-gray-900 dark:text-white line-clamp-1 text-lg tracking-wide">{item.name_product}</h3>
-                    <CategoryIcon icon={getCategoryIcon(item.category_id)} />
+                    <h3 className="font-semibold text-gray-900 dark:text-white line-clamp-1 text-lg tracking-wide">{item.product_name}</h3>
+                    <CategoryIcon icon={getCategoryIcon(item.id_category)} />
                     <p className="font-bold text-primary">${item.price}</p>
                 </div>
                 <Button
--- client/src/UI/components/home/ProductGrid.jsx
+++ client/src/UI/components/home/ProductGrid.jsx
@@ -4,8 +4,8 @@ export const ProductGrid = ({ images }) => {
     return (
         <main className="w-full p-2 ">
             <div className="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-x-4 gap-y-6 justify-items-center">
-                {images.map((image) => (
-                    <ProductCard key={image.id} item={image} />
+                {images.map((record) => (
+                    <ProductCard key={record.id_record} item={record} />
                 ))}
             </div>
         </main>
