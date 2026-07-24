ecd43f1ab22d7268d3aeef04b11648c4adbfeca3
Jhon Jairo Gelvez Gomez <jhonjairogelvezgomez@gmail.com>
2026-07-24 16:19:53 -0500

    feat(UI): value padado al input

--- client/src/UI/components/admin/Input.jsx
+++ client/src/UI/components/admin/Input.jsx
@@ -1,3 +1,14 @@
-export const Input = ({ name = "", setOnChange = null, type = "text", id = "", placeholder = "" }) => {
-    return <input required onChange={setOnChange} id={id} name={name} type={type} placeholder={placeholder} className="border border-white  rounded-lg py-1 pl-1 caret-white text-white mb-2 w-full no-spinner focus:outline-none" />;
+export const Input = ({ value = "", name = "", setOnChange = null, type = "text", id = "", placeholder = "" }) => {
+    return (
+        <input
+            required
+            value={value}
+            onChange={setOnChange}
+            id={id}
+            name={name}
+            type={type}
+            placeholder={placeholder}
+            className="border border-white  rounded-lg py-1 pl-1 caret-white text-white mb-2 w-full no-spinner focus:outline-none"
+        />
+    );
 };
